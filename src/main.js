303f34d29ae218e7f41fe6dd559d0de6b3645f8f
Piotr Wrobel <piotr.wrobel@hotmail.com>
2020-09-20 02:57:43 +0200

    Fix the keycode issue

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ Vue.config.productionTip = false;
 window.addEventListener(
   "keydown",
   function(e) {
-    if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    let keys = ["Space", "ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"];
+    if (keys.indexOf(e.key) > -1) {
       e.preventDefault();
     }
   },
